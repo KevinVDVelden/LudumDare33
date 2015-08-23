e659a5fbe8d232df1675b30ec5703a9684323e63
Kevin van der Velden <kevin.vdvelden@gmail.com>
2015-08-23 16:30:45 +0200

    Slight refactor to allow for differt attack types.

--- gamelogic/building.py
+++ gamelogic/building.py
@@ -2,70 +2,6 @@ import ecs
 import gamelogic
 import pygame
 
-class AttackComponent( ecs.Component ):
-    def __init__( self, config ):
-        self.config = config
-        self.range = config['range']
-        self.damage = config['damage']
-        self.target = None
-
-        self.costs = config['costs'] if 'costs' in config else None
-        self.attackCooldown = config['attackCooldown'] if 'attackCooldown' in config else 1
-
-        self.attackCurCooldown = 0
-
-        super().__init__( ecs.COMPONENT_ATTACK )
-
-    def think( self, ent, world ):
-        self.attackCurCooldown = max( 0, self.attackCurCooldown - 1 )
-
-        if self.costs is None:
-            storage = None
-        else:
-            storage = ent.getComponent( ecs.COMPONENT_RESOURCE )
-            if storage is None:
-                return
-
-        #Return true if we can't attack the target due it either being dead or out of range
-        def attackTarget( target ):
-            if ( target.position[0] - ent.position[0] ) ** 2 + ( target.position[1] - ent.position[1] ) ** 2 > self.range ** 2:
-                return True
-
-            while True:
-                if storage is not None:
-                    if storage.hasResources( self.costs ) and self.attackCurCooldown < 1:
-                        storage.takeResources( self.costs )
-                        self.attackCurCooldown += self.attackCooldown
-                    else:
-                        return False
-                else:
-                    if self.attackCurCooldown >= 1:
-                        return False
-                    else:
-                        self.attackCurCooldown += self.attackCooldown
-
-                health = target.getComponent( ecs.COMPONENT_HEALTH )
-                #TODO: Keep track of "teams"
-                health.takeDamage( self.damage )
-
-                if health.health < 0:
-                    return True
-
-        if self.target is not None:
-            if attackTarget( self.target ):
-                self.target = None
-
-        entities = list( world.entsInRect( pygame.Rect( ent.position[0] - self.range, ent.position[1] - self.range, self.range * 2, self.range * 2 ) ) )
-        entities.sort( key=lambda target: ( target.position[0] - ent.position[0] ) ** 2 + ( target.position[1] - ent.position[1] ) ** 2 )
-
-        for ent in entities:
-            if ent.hasComponent( ecs.COMPONENT_HEALTH ) == False:
-                continue
-
-            if attackTarget( ent ) == False:
-                self.target = ent
-
-
 def makeBuilding( self, pos, config ):
     ent = self.world.addEntity( pos )
     ent.addComponent( ecs.RenderComponent( 'img/buildings/combined/building_energy_11.png' ) )
@@ -83,7 +19,7 @@ def makeBuilding( self, pos, config ):
         self.pathFinding.addSource( pos, config['pathImportance'] )
 
     if 'attack' in config:
-        ent.addComponent( AttackComponent( config['attack'] ) )
+        ent.addComponent( gamelogic.enemies.AttackComponent( config['attack'] ) )
 
 #Resources tuple: ( use per tick, maximum stored, maximum send per burst, maximum received )
 
--- gamelogic/enemies.py
+++ gamelogic/enemies.py
@@ -1,5 +1,6 @@
 import ecs
 import random
+import pygame
 
 class PathWalker( ecs.Component ):
     def __init__( self, pathMap ):
@@ -44,3 +45,71 @@ class PathWalker( ecs.Component ):
                 self.ticksTillTarget = ticks
             else:
                 ent.velocity = ( 0, 0 )
+
+class AttackComponent( ecs.Component ):
+    def __init__( self, config ):
+        self.config = config
+        self.range = config['range']
+        self.damage = config['damage']
+        self.target = None
+
+        self.costs = config['costs'] if 'costs' in config else None
+        self.attackCooldown = config['attackCooldown'] if 'attackCooldown' in config else 1
+
+        self.attackCurCooldown = 0
+
+        super().__init__( ecs.COMPONENT_ATTACK )
+
+    def doAttack( self, target, targetHealth, world ):
+        targetHealth.takeDamage( self.damage )
+
+    def think( self, ent, world ):
+        self.attackCurCooldown = max( 0, self.attackCurCooldown - 1 )
+
+        if self.costs is None:
+            storage = None
+        else:
+            storage = ent.getComponent( ecs.COMPONENT_RESOURCE )
+            if storage is None:
+                return
+
+        #Return true if we can't attack the target due it either being dead or out of range
+        def attackTarget( target ):
+            if ( target.position[0] - ent.position[0] ) ** 2 + ( target.position[1] - ent.position[1] ) ** 2 > self.range ** 2 or target.getComponent( ecs.COMPONENT_HEALTH ).health < 0:
+                return True
+
+            while True:
+                if storage is not None:
+                    if storage.hasResources( self.costs ) and self.attackCurCooldown < 1:
+                        storage.takeResources( self.costs )
+                        self.attackCurCooldown += self.attackCooldown
+                    else:
+                        return False
+                else:
+                    if self.attackCurCooldown >= 1:
+                        return False
+                    else:
+                        self.attackCurCooldown += self.attackCooldown
+
+                health = target.getComponent( ecs.COMPONENT_HEALTH )
+                #TODO: Keep track of "teams"
+                self.doAttack( target, health, world )
+
+                if health.health < 0:
+                    return True
+
+        if self.target is not None:
+            if attackTarget( self.target ):
+                self.target = None
+
+        entities = list( world.entsInRect( pygame.Rect( ent.position[0] - self.range, ent.position[1] - self.range, self.range * 2, self.range * 2 ) ) )
+        entities.sort( key=lambda target: ( target.position[0] - ent.position[0] ) ** 2 + ( target.position[1] - ent.position[1] ) ** 2 )
+
+        for ent in entities:
+            if ent.hasComponent( ecs.COMPONENT_HEALTH ) == False:
+                continue
+
+            if attackTarget( ent ) == False:
+                self.target = ent
+
+
